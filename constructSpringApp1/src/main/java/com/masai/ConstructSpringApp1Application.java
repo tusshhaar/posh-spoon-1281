76cc6fb2d3bb1a49c5fbdc58a29c9dc7d6944d96
Tushar Trivedi <107756507+tusshhaar@users.noreply.github.com>
2022-12-13 23:02:11 +0530

    .

---
+++ constructSpringApp1/src/main/java/com/masai/ConstructSpringApp1Application.java
@@ -0,0 +1,13 @@
+package com.masai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstructSpringApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConstructSpringApp1Application.class, args);
+	}
+
+}
