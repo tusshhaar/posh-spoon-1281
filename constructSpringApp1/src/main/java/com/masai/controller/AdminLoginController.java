e3de56e4c6749b9a840b5672cd84f76a1c6c9eaa
Tushar Trivedi <107756507+tusshhaar@users.noreply.github.com>
2022-12-15 22:53:48 +0530

    Updated With Exception

--- constructSpringApp1/src/main/java/com/masai/controller/AdminLoginController.java
+++ constructSpringApp1/src/main/java/com/masai/controller/AdminLoginController.java
@@ -5,19 +5,25 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.masai.exception.AdminException;
 import com.masai.exception.AdminLoginException;
 import com.masai.exception.CustomerException;
+import com.masai.exception.PlanterException;
 import com.masai.model.AdminLoginDTO;
 import com.masai.model.Customer;
+import com.masai.model.Planter;
 import com.masai.service.AdminLoginService;
+import com.masai.service.PlanterService;
 
 import jakarta.validation.Valid;
 
@@ -55,32 +61,32 @@ public class AdminLoginController {
 		return new ResponseEntity<List<Customer>>(customers, HttpStatus.OK);
 	}
 
-@PostMapping("/{loginId}/planter")
-	public ResponseEntity<Planter> addPlanterHandler(@PathVariable("loginId")String uuid,@RequestBody Planter planter){
+    @PostMapping("/{loginId}/planter")
+	public ResponseEntity<Planter> addPlanterHandler(@PathVariable("loginId")String uuid,@RequestBody Planter planter) throws PlanterException, AdminException{
 		Planter register = pService.addPlanter(uuid, planter);
 		return new ResponseEntity<Planter>(register,HttpStatus.ACCEPTED);
 	}
 	
 	@PutMapping("/{loginId}/planter")
-	public ResponseEntity<Planter> updatePlanterHandler(@PathVariable("loginId")String uuid,@RequestBody Planter planter){
+	public ResponseEntity<Planter> updatePlanterHandler(@PathVariable("loginId")String uuid,@RequestBody Planter planter) throws PlanterException, AdminException{
 		Planter update = pService.updatePlanter(uuid, planter);
 		return new ResponseEntity<Planter>(update,HttpStatus.OK);
 	}
 	
 	@DeleteMapping("/{loginId}/planter/{planterId}")
-	public ResponseEntity<Planter> deletePlanterHandler(@PathVariable("loginId")String uuid,@PathVariable Integer planterId){
+	public ResponseEntity<Planter> deletePlanterHandler(@PathVariable("loginId")String uuid,@PathVariable Integer planterId) throws PlanterException, AdminException{
 		Planter planter = pService.deletePlanter(uuid, planterId);
 		return new ResponseEntity<Planter>(planter,HttpStatus.OK);
 	}
 	
 	@GetMapping("/{loginId}/planter/{planterId}")
-	public ResponseEntity<Planter> getPlanterByIdHandler(@PathVariable("loginId")String uuid,@PathVariable Integer planterId){
+	public ResponseEntity<Planter> getPlanterByIdHandler(@PathVariable("loginId")String uuid,@PathVariable Integer planterId) throws PlanterException, AdminException{
 		Planter planter = pService.viewPlanter(uuid,planterId);
 		return new ResponseEntity<Planter>(planter,HttpStatus.OK);
 	}
 	
 	@GetMapping("/{loginId}/planter/{shape}")
-	public ResponseEntity<Planter> getPlanterByShapeHandler(@PathVariable("loginId")String uuid, @PathVariable("shape")String shape){
+	public ResponseEntity<Planter> getPlanterByShapeHandler(@PathVariable("loginId")String uuid, @PathVariable("shape")String shape) throws PlanterException, AdminException{
 		Planter planter = pService.viewPlanterByShape(uuid,shape);
 		return new ResponseEntity<Planter>(planter,HttpStatus.OK);
 	}
