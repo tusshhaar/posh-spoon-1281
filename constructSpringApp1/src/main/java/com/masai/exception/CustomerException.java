58831706fdab0ffe64c1ac1f29baa35d096228ef
Rocker Ni3 <nitin939426@gmail.com>
2022-12-15 14:04:18 +0530

    added customer exception

--- constructSpringApp1/src/main/java/com/masai/exception/CustomerException.java
+++ constructSpringApp1/src/main/java/com/masai/exception/CustomerException.java
@@ -1,1 +1,13 @@
+package com.masai.exception;
 
+public class CustomerException extends Exception {
+	
+	public CustomerException() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public CustomerException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+}
--- constructSpringApp1/src/main/java/com/masai/model/Order.java
+++ constructSpringApp1/src/main/java/com/masai/model/Order.java
@@ -1,4 +1,4 @@
-package Model;
+package com.masai.model;
 
 public class Order {
 
