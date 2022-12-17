8de3496c150c0e11b7441ca11cd9fed6f062b972
imgyan <inex.gyan@gmail.com>
2022-12-17 16:47:43 +0530

    Bean class Update

--- constructSpringApp1/src/main/java/com/masai/model/Planter.java
+++ constructSpringApp1/src/main/java/com/masai/model/Planter.java
@@ -6,6 +6,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,17 +20,29 @@ public class Planter {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer planterId;
+	@NotNull(message = "Planter height should not be null")
+	@Min(value = 1, message = "Planter height is must be greater than equal to 1")
 	private float planterheight;
+	@NotNull(message = "Planter capacity should not be null")
+	@Min(value = 1, message = "Planter capacity is must be greater than equal to 1")
 	private Integer planterCapacity;
+	@NotNull(message = "Planter Holes should not be null")
+	@Min(value = 1, message = "Planter holes is must be greater than equal to 1")
 	private Integer drainageHoles;
+	@NotNull(message = "Planter Color should not be null")
 	private Integer planterColor;
+	@NotNull(message = "Planter Shape should not be null")
 	private String planterShape;
+	@NotNull(message = "Planter Stock should not be null")
+	@Min(value = 0, message = "Planter stock is must be greater than equal to 1")
 	private Integer planterStock;
+	@NotNull(message = "Planter Cost should not be null")
+	@Min(value = 1, message = "Planter cost is must be greater than equal to 1")
 	private Integer planterCost;
-	
+
 	@OneToOne(cascade = CascadeType.ALL)
 	private Seed seed;
-	
+
 	@OneToOne(cascade = CascadeType.ALL)
 	private Plant plant;
 }
--- constructSpringApp1/src/main/java/com/masai/model/Seed.java
+++ constructSpringApp1/src/main/java/com/masai/model/Seed.java
@@ -4,6 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,127 +16,32 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Seed {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer seedId;
+	@NotBlank(message = "Seed Name is Mandatory")
 	private String commanName;
+	@NotBlank(message = "Bloom Time is Mandatory")
 	private String bloomTime;
+	@NotBlank(message = "Watering is Mandatory")
 	private String watering;
+	@NotBlank(message = "DifficultLevel is Mandatory")
 	private String difficultLevel;
+	@NotBlank(message = "Temparature is Mandatory")
 	private String temparature;
+	@NotBlank(message = "TypeOfSeed is Mandatory")
 	private String TypeOfSeed;
+	@NotBlank(message = "Seed Description is Mandatory")
 	private String SeedDescription;
+	@NotNull(message = "stock value can not be null")
+	@Min(value =  0,message = "stock value must be greater than equal to 0")
 	private Integer SeedStock;
+	@NotNull(message = "Seed Cost should not be null")
+	@Min(value = 1 , message = "stock value must be greater than equal to 1")
 	private Double SeedsCost;
+	@NotNull(message = "Seed Per Packed should not be null")
 	private Integer SeedPerPacked;
-
-	public Seed(Integer seedId, String commanName, String bloomTime, String watering, String difficultLevel,
-			String temparature, String typeOfSeed, String seedDescription, Integer seedStock, Double seedsCost,
-			Integer seedPerPacked) {
-		super();
-		this.seedId = seedId;
-		this.commanName = commanName;
-		this.bloomTime = bloomTime;
-		this.watering = watering;
-		this.difficultLevel = difficultLevel;
-		this.temparature = temparature;
-		TypeOfSeed = typeOfSeed;
-		SeedDescription = seedDescription;
-		SeedStock = seedStock;
-		SeedsCost = seedsCost;
-		SeedPerPacked = seedPerPacked;
-	}
-	public Seed(String commanName, String bloomTime, String watering, String difficultLevel, String temparature,
-			String typeOfSeed, String seedDescription, Integer seedStock, Double seedsCost, Integer seedPerPacked) {
-		super();
-		this.commanName = commanName;
-		this.bloomTime = bloomTime;
-		this.watering = watering;
-		this.difficultLevel = difficultLevel;
-		this.temparature = temparature;
-		TypeOfSeed = typeOfSeed;
-		SeedDescription = seedDescription;
-		SeedStock = seedStock;
-		SeedsCost = seedsCost;
-		SeedPerPacked = seedPerPacked;
-	}
-	public Seed() {
-		super();
-	}
-	public Integer getSeedId() {
-		return seedId;
-	}
-	public void setSeedId(Integer seedId) {
-		this.seedId = seedId;
-	}
-	public String getCommanName() {
-		return commanName;
-	}
-	public void setCommanName(String commanName) {
-		this.commanName = commanName;
-	}
-	public String getBloomTime() {
-		return bloomTime;
-	}
-	public void setBloomTime(String bloomTime) {
-		this.bloomTime = bloomTime;
-	}
-	public String getWatering() {
-		return watering;
-	}
-	public void setWatering(String watering) {
-		this.watering = watering;
-	}
-	public String getDifficultLevel() {
-		return difficultLevel;
-	}
-	public void setDifficultLevel(String difficultLevel) {
-		this.difficultLevel = difficultLevel;
-	}
-	public String getTemparature() {
-		return temparature;
-	}
-	public void setTemparature(String temparature) {
-		this.temparature = temparature;
-	}
-	public String getTypeOfSeed() {
-		return TypeOfSeed;
-	}
-	public void setTypeOfSeed(String typeOfSeed) {
-		TypeOfSeed = typeOfSeed;
-	}
-	public String getSeedDescription() {
-		return SeedDescription;
-	}
-	public void setSeedDescription(String seedDescription) {
-		SeedDescription = seedDescription;
-	}
-	public Integer getSeedStock() {
-		return SeedStock;
-	}
-	public void setSeedStock(Integer seedStock) {
-		SeedStock = seedStock;
-	}
-	public Double getSeedsCost() {
-		return SeedsCost;
-	}
-	public void setSeedsCost(Double seedsCost) {
-		SeedsCost = seedsCost;
-	}
-	public Integer getSeedPerPacked() {
-		return SeedPerPacked;
-	}
-	public void setSeedPerPacked(Integer seedPerPacked) {
-		SeedPerPacked = seedPerPacked;
-	}
-	@Override
-	public String toString() {
-		return "Seed [seedId=" + seedId + ", commanName=" + commanName + ", bloomTime=" + bloomTime + ", watering="
-				+ watering + ", difficultLevel=" + difficultLevel + ", temparature=" + temparature + ", TypeOfSeed="
-				+ TypeOfSeed + ", SeedDescription=" + SeedDescription + ", SeedStock=" + SeedStock + ", SeedsCost="
-				+ SeedsCost + ", SeedPerPacked=" + SeedPerPacked + "]";
-	}
 
 
 }
